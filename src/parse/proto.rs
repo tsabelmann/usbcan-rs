e86e3c3663bf3660a8b4c306544bb484ddbcbc73
tsabelmann <tim.sabelmann@hotmail.de>
2026-06-17 13:25:49 +0200

    feat: Add constants

---
+++ src/parse/proto.rs
@@ -0,0 +1,21 @@
+pub mod fixed {
+    pub const HEADER1: u8 = super::variable::START;
+    pub const HEADER2: u8 = 0x55;
+    pub const TYPE:    u8 = 0x01;
+    pub const STD:     u8 = 0x01;
+    pub const EXT:     u8 = 0x02;
+    pub const DATA:    u8 = 0x01;
+    pub const REMOTE:  u8 = 0x02;
+
+    pub const RESERVED_IDX: usize = 18;
+    pub const CHECKSUM_IDX: usize = 19;
+}
+
+pub mod variable {
+    pub const START:        u8 = 0xAA;
+    pub const END:          u8 = 0x55;
+    pub const TYPE_MARKER:  u8 = 0b1100_0000;
+    pub const RTR_BIT:      u8 = 0b0001_0000;
+    pub const EXT_BIT:      u8 = 0b0010_0000;
+    pub const DLC_MASK:     u8 = 0b0000_1111;
+}
