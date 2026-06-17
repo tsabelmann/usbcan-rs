4b3f728b0d5967194798231f01aead117ff86f2b
tsabelmann <tim.sabelmann@hotmail.de>
2026-06-17 13:27:27 +0200

    feat: Add shorthand notation for encoder on Fixed and Variable type

--- src/mode.rs
+++ src/mode.rs
@@ -1,3 +1,5 @@
+use crate::parse::encode::Encoder;
+
 mod sealed {
     pub trait Sealed {}
 }
@@ -8,6 +10,18 @@ pub struct Fixed;
 impl sealed::Sealed for Fixed {}
 impl Mode for Fixed {}
 
+impl Fixed {
+    pub const fn encoder() -> Encoder<Fixed> {
+        Encoder::<Fixed>::new()
+    }
+}
+
 pub struct Variable;
 impl sealed::Sealed for Variable {}
 impl Mode for Variable {}
+
+impl Variable {
+    pub const fn encoder() -> Encoder<Variable> {
+        Encoder::<Variable>::new()
+    }
+}
