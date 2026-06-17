415b16c0b257ad79d91115f0b2b7313724647ac0
tsabelmann <tim.sabelmann@hotmail.de>
2026-06-17 13:27:43 +0200

    feat: Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,10 @@
 #![no_std]
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
 pub mod frame;
 pub mod id;
 pub mod mode;
-pub mod parser;
-
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+pub mod parse;
