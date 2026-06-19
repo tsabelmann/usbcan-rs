e4541a8b1cc3d8180a0ddda15cc46c3d6f4aadc1
tsabelmann <tim.sabelmann@hotmail.de>
2026-06-19 15:42:36 +0200

    init: Add tcpstream.rs

---
+++ examples/tcpstream.rs
@@ -0,0 +1,43 @@
+use std::{io::Read, net::{Ipv4Addr, SocketAddr, TcpStream}, thread::sleep, time::{self, Duration}};
+
+use embedded_io_adapters::std::FromStd;
+use usbcan::{frame::Frame, id::{CanId, ExtendedId}, mode::Variable};
+use usbcan::interface::Interface;
+
+fn main() {
+    // let addr = SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::from_octets([10, 0, 1, 5])), 2001);
+    // let mut stream = TcpStream::connect(addr).expect("could not connect to stream");
+    // let mut buf = [0x00; 20];
+
+    // let mut decoder = Variable::decoder();
+    // let mut idx = 0usize;
+    // loop {
+    //     if let Ok(size) = stream.read(&mut buf) {
+    //         // println!("size = {:?}", size);
+    //         for frame in decoder.decode_slice(&buf[..size]) {
+    //             if let Ok(f) = frame {
+    //                 println!("{:08?} {:?}", idx, f);
+    //                 idx += 1;
+    //             }
+    //         }
+    //     }
+    // }
+
+    let addr = SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::from_octets([10, 0, 1, 5])), 2001);
+    let stream = TcpStream::connect(addr).expect("could not connect to stream");
+    let mut interface: Interface<FromStd<TcpStream>, Variable, usbcan::interface::Synch> = Interface::new_sync(FromStd::new(stream));
+    
+    let my_frame = Frame::new(CanId::Extended(ExtendedId::new(0x11223345).unwrap()), &[0x11, 0x22, 0x33, 0x44]).unwrap();
+    
+    loop {
+        // if let Ok(frame) = interface.recv() {
+        //     println!("{:?}", frame);
+        // }
+
+        match interface.try_send(&my_frame) {
+            Ok(_) => println!("Ok"),
+            Err(err) => println!("{:?}", err),
+        }
+        sleep(Duration::from_secs(1));
+    }
+}
