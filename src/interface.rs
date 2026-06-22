6b97670cb794d7a94efe9a6fec267f25e17f2360
tsabelmann <tim.sabelmann@hotmail.de>
2026-06-22 08:30:14 +0200

    refact: Remove unnecessary imports

--- src/interface.rs
+++ src/interface.rs
@@ -1,6 +1,5 @@
-use crate::{frame::Frame, mode::Mode, parse::{decode::{Decoder, DecoderError, PushDecode}, encode::{Encode, Encoder, EncoderError}}};
+use crate::{config::Config, frame::Frame, mode::Mode, parse::{decode::{Decoder, DecoderError, PushDecode}, encode::{ConfigEncoder, Encode, Encoder, EncoderError}}};
 use core::marker::PhantomData;
-use std::hint::black_box;
 
 use embedded_io::{Read as SynchRead, Write as SynchWrite};
 use embedded_io_async::{Read as AsynchRead, Write as AsynchWrite};
@@ -40,7 +39,11 @@ where
     _io: PhantomData<IO>
 }
 
-impl<T: SynchRead + SynchWrite, M: Mode> Interface<T, M, Synch> {
+impl<T: SynchRead + SynchWrite, M: Mode> Interface<T, M, Synch> 
+where 
+    Encoder<M>: Encode<Frame>,
+    Decoder<M>: PushDecode
+{
     pub fn new_sync(io: T) -> Interface<T, M, Synch> {
         Interface {
             io, 
@@ -52,11 +55,25 @@ impl<T: SynchRead + SynchWrite, M: Mode> Interface<T, M, Synch> {
         }
     }
 
+    pub fn configure(&mut self, config: Config) -> Result<(), InterfaceError<T::Error>> {
+        let mut buf = [0u8; 20];
+        let mut config_encoder = ConfigEncoder;
 
-    pub fn try_send(&mut self, frame: &Frame) -> Result<(), InterfaceError<T::Error>> 
-    where 
-        Encoder<M>: Encode
-    {
+        // encode
+        let to_write = config_encoder.encode(&config, &mut buf).map_err(InterfaceError::Encode)?;
+
+        // send
+        let mut written = 0;
+        while written < to_write {
+            match self.io.write(&buf[written..]) {
+                Ok(size) => written += size,
+                Err(err) => return Err(InterfaceError::Io(err)),
+            }
+        }
+        Ok(())
+    }
+
+    pub fn try_send(&mut self, frame: &Frame) -> Result<(), InterfaceError<T::Error>> {
         let n = self.encoder.encode(frame, &mut self.send_buf).map_err(InterfaceError::Encode)?;
         println!("buf = {:#02X?}", &self.send_buf[..n]);
         println!("n = {:?}", n);
@@ -65,10 +82,7 @@ impl<T: SynchRead + SynchWrite, M: Mode> Interface<T, M, Synch> {
         Ok(())
     }
 
-    pub fn recv(&mut self) -> Result<Frame, InterfaceError<T::Error>>
-    where 
-        Decoder<M>: PushDecode
-    {
+    pub fn recv(&mut self) -> Result<Frame, InterfaceError<T::Error>>{
         loop {
             match self.try_recv() {
                 Ok(None) => continue,
@@ -78,10 +92,7 @@ impl<T: SynchRead + SynchWrite, M: Mode> Interface<T, M, Synch> {
         }
     }
 
-    pub fn try_recv(&mut self) -> Result<Option<Frame>, InterfaceError<T::Error>>
-    where 
-        Decoder<M>: PushDecode
-    {
+    pub fn try_recv(&mut self) -> Result<Option<Frame>, InterfaceError<T::Error>> {
         let n = self.io.read(&mut self.recv_buf).map_err(InterfaceError::Io)?;
         let mut result = None;
         for iter in self.decoder.decode_slice(&self.recv_buf[..n]){
